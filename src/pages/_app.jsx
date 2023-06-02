6f520cf982659947986cbe79102700734a6cbc22
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-06-02 15:47:06 -0700

    allow override of CIO region in localhost storage

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import 'focus-visible'
 import '@/styles/tailwind.css'
 const SITE_ID = process.env.CIO_SITE_ID;
+const REGION = Boolean(process.env.CIO_REGION_US) == true ? "US" : "EU";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -29,8 +30,12 @@ export default function App({ Component, pageProps }) {
   <Script id="cio-tracker" strategy="beforeInteractive">
       {`
         let siteID = "${SITE_ID}";
+        let region = "${REGION}";
         const overrideSiteID = window.localStorage.getItem("CX_SITE_CIO_SITE_ID");
         if (overrideSiteID && overrideSiteID != "") siteID = overrideSiteID;
+        const overrideRegion = window.localStorage.getItem("CX_SITE_CIO_REGION");
+        if (overrideRegion && overrideRegion != "") region = overrideRegion;
+
         var _cio = _cio || [];
         (function() {
             var a,b,c;a=function(f){return function(){_cio.push([f].
@@ -45,10 +50,9 @@ export default function App({ Component, pageProps }) {
             
             //Enables in-app messaging
             t.setAttribute('data-use-in-app', 'true');
-            
-            t.src = 'https://assets.customer.io/assets/track.js';
-            //If your account is in the EU, use:
-            //t.src = 'https://assets.customer.io/assets/track-eu.js'
+
+            if (region != "EU" ) t.src = 'https://assets.customer.io/assets/track.js';
+            else t.src = 'https://assets.customer.io/assets/track-eu.js' // Will connect with EU datacenter if your account is in the EU region
             s.parentNode.insertBefore(t, s);
         })();
   `}
