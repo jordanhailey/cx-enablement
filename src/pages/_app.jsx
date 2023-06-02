d144b2fffdaf7beeb040b55706d54cda12941b37
Jordan Hailey <jordan.m.hailey@gmail.com>
2023-06-02 13:54:38 -0700

    add cio JS snippet to site

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,34 @@
+import Script from 'next/script';
 import 'focus-visible'
 import '@/styles/tailwind.css'
+const SITE_ID = process.env.CIO_SITE_ID
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Script id="cio-tracker" strategy="beforeInteractive">
+      {`
+        var _cio = _cio || [];
+        (function() {
+            var a,b,c;a=function(f){return function(){_cio.push([f].
+            concat(Array.prototype.slice.call(arguments,0)))}};b=["load","identify",
+            "sidentify","track","page","on","off"];for(c=0;c<b.length;c++){_cio[b[c]]=a(b[c])};
+            var t = document.createElement('script'),
+                s = document.getElementsByTagName('script')[0];
+            t.async = true;
+            t.id    = 'cio-tracker';
+            t.setAttribute('data-site-id', '${SITE_ID}');
+            t.setAttribute('data-use-array-params', 'true');
+            
+            //Enables in-app messaging
+            t.setAttribute('data-use-in-app', 'true');
+            
+            t.src = 'https://assets.customer.io/assets/track.js';
+            //If your account is in the EU, use:
+            //t.src = 'https://assets.customer.io/assets/track-eu.js'
+            s.parentNode.insertBefore(t, s);
+        })();
+  `}
+  </Script>
+  <Component {...pageProps} />
+  </>
 }
